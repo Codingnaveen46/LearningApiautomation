94547c29e12160d467d22bc233cecf3c2ba39845
Codingnaveen46 <naveenreddy4411@gmail.com>
2025-03-25 10:20:01 +0530

    added new lines of code

--- tests/Postrequest.spec.js
+++ tests/Postrequest.spec.js
@@ -1,3 +1,13 @@
 import {test,expect} from '@playwright/test'
+import { request } from 'http'
 
+test('this is the testapi from playwright',async({request})=>{
+
+    const response = await request.post('url',{
+
+        headers:{
+            
+        }
+    })
+})
 
